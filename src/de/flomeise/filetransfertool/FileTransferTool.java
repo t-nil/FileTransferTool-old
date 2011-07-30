5a2e3016eeed898e843be4d64dfda5578c8f84c3
Mars <oliver214@web.de>
2011-07-30 16:07:54 +0200

    Removed unused imports in FileTransferTool.java

--- src/de/flomeise/filetransfertool/FileTransferTool.java
+++ src/de/flomeise/filetransfertool/FileTransferTool.java
@@ -5,7 +5,6 @@
 package de.flomeise.filetransfertool;
 
 import com.twmacinta.util.MD5;
-import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
@@ -13,7 +12,6 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
